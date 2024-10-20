2444de51ba2d3ff0a4dae3db073000f6a4f44cee
Douglas Rezende <33718006+douglasrezende@users.noreply.github.com>
2024-10-20 16:04:22 -0300

    Create string.java

---
+++ string.java
@@ -0,0 +1,22 @@
+// Java program to get a specific character
+// from a given String at a specific index
+class Test{
+
+    // Driver code
+    public static void main(String[] args)
+    {
+
+        // Get the String
+        String str = "Douglas";
+
+        // Get the index
+        int index = 5;
+
+        // Get the specific character
+        char ch = str.charAt(index);
+
+        System.out.println("Character from " + str
+                           + " at index " + index
+                           + " is " + ch);
+    }
+}
